5f8e40470b174bf7de7eaaa43d8cc19cc5c80490
Thalles <thallesrafaell@outlook.com>
2025-03-03 00:21:59 -0300

    refactor: update reducer to handle additional action types and improve state management

--- src/context/reducer.ts
+++ src/context/reducer.ts
@@ -2,54 +2,73 @@
 
 // Tipando o estado (State)
 export interface Task {
-    id: number;
-    name: string;
-    done: boolean;
-    createdAt: string;
-    updatedAt?: string;
-    priority: "Urgente"| "Alta" | "Média" | "Baixa";
-    important: boolean;
-    category: string;
+  id: number;
+  name: string;
+  done: boolean;
+  createdAt: string;
+  updatedAt?: string;
+  priority: "Urgente" | "Alta" | "Média" | "Baixa";
+  important: boolean;
+  category: string;
+}
+
+interface State {
+  addVisible: boolean;
+  tasks: Task[];
+}
+
+// Definindo as ações possíveis (Action)
+interface Action {
+  type: string;
+  payload?: any; // Pode ser qualquer tipo, mas no caso de ADD_TASK e REMOVE_TASK, é um Task ou id
+}
+
+// Estado inicial
+export const initialState: State = {
+  addVisible: false,
+  tasks: [],
+};
+
+// Tipos das ações
+export const actionTypes = {
+  ADD_TASK: "ADD_TASK",
+  REMOVE_TASK: "REMOVE_TASK",
+  EDIT_TASK: "EDIT_TASK",
+  GET_TASK_BY_ID: "GET_TASK_BY_ID",
+} as const;
+
+// Redefinindo o reducer com o tipo correto de estado e ação
+export const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case actionTypes.ADD_TASK:
+      return {
+        ...state,
+        tasks: [...state.tasks, action.payload],
+      };
+    case actionTypes.REMOVE_TASK:
+      return {
+        ...state,
+        tasks: state.tasks.filter((task) => task.id !== action.payload),
+      };
+    case actionTypes.EDIT_TASK:
+      return {
+        ...state,
+        tasks: state.tasks.map((task) => {
+          if (task.id === action.payload.id) {
+            return {
+              ...task,
+              ...action.payload,
+            };
+          }
+          return task;
+        }),
+      };
+    case actionTypes.GET_TASK_BY_ID:
+      return {
+        ...state,
+        tasks: state.tasks.filter((task) => task.id === action.payload),
+      };
+    default:
+      return state;
   }
-  
-  interface State {
-    addVisible: boolean;
-    tasks: Task[];
-  }
-  
-  // Definindo as ações possíveis (Action)
-  interface Action {
-    type: string;
-    payload?: any;  // Pode ser qualquer tipo, mas no caso de ADD_TASK e REMOVE_TASK, é um Task ou id
-  }
-  
-  // Estado inicial
-  export const initialState: State = {
-    addVisible: false,
-    tasks: [],
-  };
-  
-  // Tipos das ações
-  export const actionTypes = {
-    ADD_TASK: "ADD_TASK",
-    REMOVE_TASK: "REMOVE_TASK",
-  } as const;
-  
-  // Redefinindo o reducer com o tipo correto de estado e ação
-  export const reducer = (state: State, action: Action): State => {
-    switch (action.type) {      
-      case actionTypes.ADD_TASK:
-        return {
-          ...state,
-          tasks: [...state.tasks, action.payload],
-        };
-      case actionTypes.REMOVE_TASK:
-        return {
-          ...state,
-          tasks: state.tasks.filter(task => task.id !== action.payload),
-        };
-      default:
-        return state;
-    }
-  };
-  
+};
